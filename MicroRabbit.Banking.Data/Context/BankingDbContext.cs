78831198a9b0872993616d6fbca0c0f011357604
nilayshah80 <nilay.shah3nov@gmail.com>
2019-08-27 23:25:56 +0530

    Data, Domain, Application, and API

---
+++ MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
