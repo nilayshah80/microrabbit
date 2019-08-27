000e87fcf47a24ab1ffb019b299f35d22741b216
nilayshah80 <nilay.shah3nov@gmail.com>
2019-08-27 22:28:54 +0530

    Added Infra

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void ResgisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
